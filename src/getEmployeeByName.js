6f90c87446df9ccf7ac5961ae624ef902d43f2b3
AlineMSant <alinems4120@gmail.com>
2022-12-08 17:49:00 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+const getEmployeeByName = (name) => {
+  const arrayEmployees = data.employees;
+  if (!name) {
+    return {};
+  }
+  return arrayEmployees.find((obj) => obj.firstName === name || obj.lastName === name);
 };
 
 module.exports = getEmployeeByName;
