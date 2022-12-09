3cc9df70472a5b8a52cf6cb5f249031d34e38d3f
AlineMSant <alinems4120@gmail.com>
2022-12-08 22:27:23 -0300

    requisito 4, função getRelatedEmployees melhorado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -11,11 +11,9 @@ const isManager = (id) => {
 
 const getRelatedEmployees = (managerId) => {
   if (isManager(managerId) === true) {
-    const arrayEmployees = data.employees;
     const array = [];
-    arrayEmployees.forEach((obj) => {
-      const arrayManagers = obj.managers;
-      arrayManagers.forEach((element) => {
+    data.employees.forEach((obj) => {
+      obj.managers.forEach((element) => {
         if (element === managerId) {
           array.push(`${obj.firstName} ${obj.lastName}`);
         }
@@ -26,6 +24,6 @@ const getRelatedEmployees = (managerId) => {
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
-// console.log(getRelatedEmployees('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
