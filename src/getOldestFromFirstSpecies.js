a4502875e83c2ce977cc651808c72dfaf824e03b
AlineMSant <alinems4120@gmail.com>
2022-12-12 20:21:24 -0300

    tentando passr com erro lint corrigido requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,16 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-//   const idFirstAnimal = data.employees.find((element) => element.id === id).responsibleFor[0];
-//   const arrayResidents = data.species.find((element) => element.id === idFirstAnimal).residents;
-//   const objOldestAnimal = arrayResidents.reduce((acc, curr) => acc.age > curr.age ? acc : curr);
+  const idFirstAnimal = data.employees.find((element) => element.id === id).responsibleFor[0];
+  const arrayResidents = data.species.find((element) => element.id === idFirstAnimal).residents;
+  const objOldestAnimal = arrayResidents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
 
-  //   const retorno = [];
-  //   retorno.push(objOldestAnimal.name);
-  //   retorno.push(objOldestAnimal.sex);
-  //   retorno.push(objOldestAnimal.age);
+  const retorno = [];
+  retorno.push(objOldestAnimal.name);
+  retorno.push(objOldestAnimal.sex);
+  retorno.push(objOldestAnimal.age);
 
-//   return retorno;
+  return retorno;
 };
 
 module.exports = getOldestFromFirstSpecies;
