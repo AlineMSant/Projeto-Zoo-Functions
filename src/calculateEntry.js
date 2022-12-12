00622a78f7f405e90460713fc85267f2dde44d72
AlineMSant <alinems4120@gmail.com>
2022-12-12 17:57:14 -0300

    requsitio 8 falta retorna 0 quando receber ojeto vazio e ajustar o lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
+// const entrant = [
+//   { age: 5 },
+//   { age: 50 },
+// ];
+
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.reduce((acc, curr) => (curr.age < 18 ? acc += 1 : acc = acc), 0);
+
+  const adult = entrants.reduce((acc, curr) => (curr.age >= 18 ? acc += 1 && curr.age < 50 : acc = acc), 0);
+
+  const senior = entrants.reduce((acc, curr) => (curr.age >= 50 ? acc += 1 : acc = acc), 0);
+
+  return {
+    adult,
+    child,
+    senior,
+  };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  let retorno;
+  if (!entrants) {
+    retorno = 0;
+  } else if (entrants === {}) {
+    retorno = 0;
+  } else {
+    const totalChild = countEntrants(entrants).child * data.prices.child;
+    const totalAdult = countEntrants(entrants).adult * data.prices.adult;
+    const totalSenior = countEntrants(entrants).senior * data.prices.senior;
+    retorno = totalChild + totalAdult + totalSenior;
+  }
+  return retorno;
 };
 
+// console.log(calculateEntry(entrant));
 module.exports = { calculateEntry, countEntrants };
