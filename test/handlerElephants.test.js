dcd9f397dee285c88788b303d7fd87178363c892
AlineMSant <alinems4120@gmail.com>
2022-12-12 16:00:35 -0300

    requsito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -9,24 +9,24 @@ describe('Testes da função HandlerElephants', () => {
     expect(handlerElephants('names', 'elephants')).toStrictEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   });
 
-  it('Verifica se a função retorna corretamente quando recebe paramtro averageAge', () => {
+  it('Verifica se a função retorna corretamente quando recebe parametro averageAge', () => {
     expect(handlerElephants('averageAge', 'elephants')).toStrictEqual(10.5); // não precisou da lógica aproximada pois o resultado é exatamente 10.5
   });
 
-  it('', () => {
-
+  it('Verifica se a função retorna corretamente quando recebe parametro location', () => {
+    expect(handlerElephants('location', 'elephants')).toStrictEqual('NW');
   });
 
-  it('', () => {
-
+  it('Verifica se a função retorna corretamente quando recebe parametro popularity', () => {
+    expect(handlerElephants('popularity', 'elephants')).toStrictEqual(5);
   });
 
-  it('', () => {
-
+  it('Verifica se a função retorna corretamente quando recebe parametro availability', () => {
+    expect(handlerElephants('availability', 'elephants')).toStrictEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
   });
 
-  it('', () => {
-
+  it('Verifica se a função retorna corretamente quando NÃO recebe parametro', () => {
+    expect(handlerElephants()).toStrictEqual(undefined);
   });
 });
 
