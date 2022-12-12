db4b16dc71e8a7b95d80f94c9f0d853a4ebb4086
AlineMSant <alinems4120@gmail.com>
2022-12-12 19:22:45 -0300

    requsito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const idFirstAnimal = data.employees.find((element) => element.id === id).responsibleFor[0];
+  const arrayResidents = data.species.find((element) => element.id === idFirstAnimal).residents;
+  const objOldestAnimal = arrayResidents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+
+  const retorno = [];
+  retorno.push(objOldestAnimal.name);
+  retorno.push(objOldestAnimal.sex);
+  retorno.push(objOldestAnimal.age);
+
+  return retorno;
 };
 
 module.exports = getOldestFromFirstSpecies;
