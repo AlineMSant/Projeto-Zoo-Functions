1748c7258b600a074282381d06188339190efdb2
AlineMSant <alinems4120@gmail.com>
2022-12-12 15:03:47 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+// anotações dia-03-filter-reduce 'para incrementar valores dentro de um objeto a partir de uma array'
+const ifNoParameter = () => data.species.reduce((acc, curr) => {
+  if (!acc[curr]) {
+    acc[curr.name] = curr.residents.length;
+  }
+
+  return acc;
+}, {});
+
+const countAnimals = (obj) => {
+  if (!obj) {
+    return ifNoParameter();
+  }
+
+  if (!obj.sex) {
+    return data.species.find((element) => element.name === obj.species).residents.length;
+  }
+  const arrayResidents = data.species.find((element) => element.name === obj.species).residents;
+  return arrayResidents.filter((resident) => resident.sex === obj.sex).length;
 };
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
