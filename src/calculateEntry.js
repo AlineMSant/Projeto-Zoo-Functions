7ccff6a4d069558fcb1c1e598796449ee327ca5f
AlineMSant <alinems4120@gmail.com>
2022-12-13 15:24:56 -0300

    requsito 8 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -6,24 +6,23 @@ const data = require('../data/zoo_data');
 // ];
 
 const countEntrants = (entrants) => {
-  // const child = entrants.reduce((acc, curr) => (curr.age < 18 ? acc += 1 : acc = acc), 0);
+  const child = entrants.reduce((acc, curr) => (curr.age < 18 ? acc + 1 : acc), 0);
 
-  // const adult = entrants.reduce((acc, curr) => (curr.age >= 18 && curr.age < 50 ? acc += 1 : acc = acc), 0);
+  const adult = entrants.reduce((acc, curr) =>
+    (curr.age >= 18 && curr.age < 50 ? acc + 1 : acc), 0);
 
-  // const senior = entrants.reduce((acc, curr) => (curr.age >= 50 ? acc += 1 : acc = acc), 0);
+  const senior = entrants.reduce((acc, curr) => (curr.age >= 50 ? acc + 1 : acc), 0);
 
-  // return {
-  //   adult,
-  //   child,
-  //   senior,
-  // };
+  return {
+    adult,
+    child,
+    senior,
+  };
 };
 
 const calculateEntry = (entrants) => {
   let retorno;
-  if (!entrants) {
-    retorno = 0;
-  } else if (entrants === {}) {
+  if (!entrants || Object.keys(entrants).length === 0) {
     retorno = 0;
   } else {
     const totalChild = countEntrants(entrants).child * data.prices.child;
