ead68f77f5bfdcf64cd956139361d912c9037055
AlineMSant <alinems4120@gmail.com>
2022-12-13 16:53:24 -0300

    estrutura do requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeeByName = () => { // deve retornar por firstName ou lastName
+
+  // return {
+  //   id: "4b40a139-d4dc-4f09-822d-ec25e819a5ad", // id da pessoa
+  //   fullName: "Sharonda Spry", // nome completo: firstName + lastName
+  //   species: [ "otters", "frogs" ], // espécies as quais a pessoa é responsável
+  //   locations: [ "SE", "SW" ], // Um array contendo todas as localizações das espécies
+  // }
+};
+
+const getEmployeeById = () => {};
+
+const getAllEmployees = () => {}; // retornar uma array de objetos.
+
+const getEmployeesCoverage = (obj) => {
+  let retorno;
+  if (Object.keys(obj).length === 0) {
+    retorno = getAllEmployees();
+  } else if (!obj.id) {
+    retorno = getEmployeeByName();
+  } else if (!obj.name) {
+    retorno = getEmployeeById();
+  } else {
+    // retorno = throw new Error('Informação inválida') //caso não encontre employee correspondente ao id, lastName ou fristName.
+  }
+  return retorno;
 };
 
 module.exports = getEmployeesCoverage;
